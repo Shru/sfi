64c6a043087218ee66d2f75f58b0706ff7e678d0
Shru <shrut.g@gmail.com>
2025-05-08 22:59:35 -0400

    Modified hardhat config to remove deprecated Goerli support

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,14 +5,6 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    base_goerli: {
-      url: "https://goerli.base.org",
-      accounts: [process.env.PRIVATE_KEY]
-    },
-    goerli: {
-      url: "https://rpc.ankr.com/eth_goerli",
-      accounts: [process.env.PRIVATE_KEY]
-    },
     sepolia_base: {
       url: "https://sepolia.base.org",
       accounts: [process.env.PRIVATE_KEY]
